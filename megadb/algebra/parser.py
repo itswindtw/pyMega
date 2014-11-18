fb55282cb228013ced300f21642b775a6400aa4d
Meng-Hsin Tung <itswindtw@gmail.com>
2014-11-17 20:04:23 -0500

    Improve parser code quality

--- megadb/algebra/parser.py
+++ megadb/algebra/parser.py
@@ -1,57 +1,80 @@
 import sqlparse
 import sqlparse.sql as sql
 
-from plan import *
+from megadb.tree import TreeNode
+from megadb.algebra.plan import Field, Comparison, Relation
+from megadb.algebra.plan import Projection, Selection, CartesianProduct
 
-def parse_sql(sql):
-    # TODO: parse validation
-    parsed = sqlparse.parse(sql)[0]
+def parse_sql(sql_str):
+    # TODO: parsing validation
+    parsed = sqlparse.parse(sql_str)[0]
     return parse_select(parsed)
 
 def parse_select(stmt):
-    # SELECT (1: fields) FROM (2: tables) WHERE (3: where)
-    assert(stmt.get_type() == 'SELECT')
+    """Parse SQL selection statement.
+
+    Args:
+        stmt: SELECT [fields] FROM [table_names] WHERE [where_clause]
+    Return:
+        A logical tree
+    """
+    def extract_parts(tokens):
+        # fields
+        fields = tokens.token_next(0)
+        inx = tokens.token_index(fields)
+        # table_names
+        from_kw = tokens.token_next_match(inx, sqlparse.tokens.Keyword, 'FROM')
+        inx = tokens.token_index(from_kw)
+        table_names = tokens.token_next(inx)
+        # where_clause
+        where_clause = tokens.token_next_by_instance(inx, sql.Where)
+
+        return fields, table_names, where_clause
+
+    def construct_tree(fields, table_names, where_clause):
+        node = None
+
+        # Projection
+        if isinstance(fields, sql.Identifier):
+            node = Projection(node, [Field(str(fields))])
+        elif isinstance(fields, sql.IdentifierList):
+            node = Projection(
+                node, [Field(str(f)) for f in fields.get_identifiers()])
+        else:
+            node = Projection(node, []) # represent '*'
+
+        # Selection
+        if where_clause is not None:
+            conds = parse_where_clause(where_clause)
+            node = Selection(node, conds)
+
+        # Relation or CartesianProduct
+        if isinstance(table_names, sql.Identifier):
+            node = Relation(node, str(table_names))
+        elif isinstance(table_names, sql.IdentifierList):
+            node = parse_relations(node, table_names.get_identifiers())
+
+        # find root of this tree
+        while node.parent:
+            node = node.parent
+
+        return node
+
+    assert stmt.get_type() == 'SELECT'
     tokens = sql.TokenList(stmt.tokens)
 
-    # 1
-    fields = tokens.token_next(0)
-    inx = tokens.token_index(fields)
-    # 2
-    from_kw = tokens.token_next_match(inx, sqlparse.tokens.Keyword, 'FROM')
-    inx = tokens.token_index(from_kw)
-    tables = tokens.token_next(inx)
-    # 3
-    where_clause = tokens.token_next_by_instance(inx, sql.Where)
+    # extract parts from tokens
+    fields, table_names, where_clause = extract_parts(tokens)
 
-    # Tree Construction
-    node = None
-
-    if isinstance(fields, sql.Identifier):
-        node = Projection(node, [Field(str(fields))])
-    elif isinstance(fields, sql.IdentifierList):
-        node = Projection(node, [Field(str(f)) for f in fields.get_identifiers()])
-    else:
-        node = Projection(node, []) # represent '*'
-
-    if where_clause is not None:
-        conds = parse_where_clause(where_clause)
-        node = Selection(node, conds)
-
-    if isinstance(tables, sql.Identifier):
-        node = Relation(node, str(tables))
-    elif isinstance(tables, sql.IdentifierList):
-        node = parse_relations(node, tables.get_identifiers())
-
-    while node.parent:
-        node = node.parent
-
-    return node
+    # construction of logical tree
+    return construct_tree(fields, table_names, where_clause)
 
 def parse_relations(parent, ids):
-    def combine(x, y):
+    """Fold relations into join nodes."""
+    def combine(acc, relation):
         node = CartesianProduct(None)
-        x.parent = node
-        Relation(node, y)
+        acc.parent = node
+        Relation(node, relation)
         return node
 
     head = Relation(None, next(ids))
@@ -60,52 +83,48 @@ def parse_relations(parent, ids):
     return join_tree
 
 def parse_where_clause(clause):
+    """Parse where_clause. It supports only AND conditions"""
     tokens = sql.TokenList(clause.tokens)
-
     conds = []
+
     inx = 0
     cond = tokens.token_next_by_instance(inx, sql.Comparison)
     while cond:
-        if cond.is_group():
-            conds.append(parse_conditions(cond))
-        else:
-            # FIXME: unnecessary now?
-            x = tokens.token_prev(tokens.token_index(cond))
-            y = tokens.token_next(tokens.token_index(cond))
-            conds.append(Comparison(x, y, cond))
+        conds.append(parse_condition(cond))
 
         inx = tokens.token_index(cond)
         cond = tokens.token_next_by_instance(inx+1, sql.Comparison)
 
     return conds
 
-def parse_conditions(cond):
+def parse_condition(cond):
+    """Parse [field] [operator] [value]"""
     tokens = sql.TokenList(cond.tokens)
 
     field_token = tokens.token_first()
     if isinstance(field_token, sql.Identifier):
-        x = Field(str(field_token))
+        field = Field(str(field_token))
     else:
-        x = str(field_token)
+        field = str(field_token)
 
     comp = tokens.token_next(tokens.token_index(field_token))
 
-
     value_token = tokens.token_next(tokens.token_index(comp))
     if isinstance(value_token, sql.Identifier):
-        y = Field(str(value_token))
+        value = Field(str(value_token))
     elif value_token.ttype == sqlparse.tokens.String.Single:
-        y = str(value_token)[1:-1]
+        value = str(value_token)[1:-1]
     else:
-        y = str(value_token)
+        value = str(value_token)
 
-    return Comparison(x, y, str(comp))
+    return Comparison(field, value, str(comp))
 
 def print_parse_tree(root):
+    """A helper to print out logical tree."""
     def aux(node, level):
         print '  '*level + repr(node)
         if isinstance(node, TreeNode):
-            for c in node.children:
-                aux(c, level+1)
+            for child in node.children:
+                aux(child, level+1)
     aux(root, 0)
     print
--- megadb/tests/algebra/test_parser.py
+++ megadb/tests/algebra/test_parser.py
@@ -3,19 +3,15 @@ from megadb.algebra.parser import parse_sql, print_parse_tree
 
 class ParserTestCase(unittest.TestCase):
     def test_parse_sql(self):
+        def print_and_parse(stmt):
+            print stmt
+            tree = parse_sql(stmt)
+            print_parse_tree(tree)
+
         print
 
-        tree = parse_sql("SELECT title, a FROM StarsIn WHERE starName = name AND movieYear = 2008")
-        print_parse_tree(tree)
-
-        tree = parse_sql("SELECT * FROM StarsIn, MovieStar, SomeTable WHERE starName = name AND birthAt = 2014")
-        print_parse_tree(tree)
-
-        tree = parse_sql("SELECT office FROM Students, Depts WHERE Students.dept = Depts.name AND Students.name='Smith'")
-        print_parse_tree(tree)
-
-        tree = parse_sql("SELECT R.B,D FROM R,S WHERE R.A='c' AND S.E=2 AND R.C = S.C")
-        print_parse_tree(tree)
-
-        tree = parse_sql("SELECT office FROM Students, Depts WHERE Students.dept = Depts.name")
-        print_parse_tree(tree)
+        print_and_parse("SELECT title, a FROM StarsIn WHERE starName = name AND movieYear = 2008")
+        print_and_parse("SELECT * FROM StarsIn, MovieStar, SomeTable WHERE starName = name AND birthAt = 2014")
+        print_and_parse("SELECT office FROM Students, Depts WHERE Students.dept = Depts.name AND Students.name='Smith'")
+        print_and_parse("SELECT R.B,D FROM R,S WHERE R.A='c' AND S.E=2 AND R.C = S.C")
+        print_and_parse("SELECT office FROM Students, Depts WHERE Students.dept = Depts.name")
